2af308defdbb2cb160225384f0c47e31a5e0eeb4
mikaelemekenese <m.mekenese@cci-formation.nc>
2021-08-12 16:01:53 +1100

    new commit

---
+++ classes/database.php
@@ -0,0 +1,55 @@
+<?php
+
+class Database {
+
+    const servername = "localhost";
+    const username = "root";
+    const password = "";
+    const database = "sakila";
+
+    public static $conn;
+
+    protected static function connect() {
+        try {
+            if (empty(self::$conn)) {
+                $conn = new PDO("mysql:host=" . self::servername . ";dbname=" . self::database, self::username, self::password);
+                $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                self::$conn = $conn;
+            }
+
+            return self::$conn;
+        } catch(PDOException $e) {
+            echo $e->getMessage();
+        }
+    }
+
+    protected static function query($sql, $params = []) {
+        try {
+            $stmt = self::connect()->prepare($sql);
+            $stmt->setFetchMode(PDO::FETCH_ASSOC);
+            $stmt->execute($params);
+
+            return $stmt;
+        } catch(PDOException $e) {
+            echo $e->getMessage();
+        }
+    }
+
+    protected static function insert($table, $params) {
+        $columns = array_keys($params);
+        $queries = array_reduce($columns, function($prev, $next) {
+            return $prev . ($prev ? ', ' : '') . $next;
+        }, '');
+        $values = array_reduce($columns, function($prev, $next) {
+            return $prev . ($prev ? ', ' : '') . ':' . $next;
+        }, '');
+        $stmt = self::query("INSERT INTO $table ($queries) VALUES ($values)", $params);
+
+        return self::$conn->lastInsertId();
+    }
+
+    protected static function fetchAll() {
+        $films = self::query('SELECT * FROM films');
+        return $films->fetchAll();
+    }
+}
